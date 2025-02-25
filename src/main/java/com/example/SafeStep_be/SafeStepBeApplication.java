f84858783daa313f11879dfe3aeaa437c86af335
petruselgeorge1 <petruselgeorge@gmail.com>
2025-02-25 11:33:51 +0200

    -m first commit

---
+++ src/main/java/com/example/SafeStep_be/SafeStepBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SafeStep_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeStepBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeStepBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SafeStep_be/SafeStepBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SafeStep_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeStepBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
