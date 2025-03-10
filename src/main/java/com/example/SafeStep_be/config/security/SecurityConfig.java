b5a69158e97f21e879a3652f0600496161c1198d
petru <petruselgeorge@gmail.com>
2025-03-10 13:12:52 +0200

    -started security cfg

---
+++ src/main/java/com/example/SafeStep_be/config/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.SafeStep_be.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        httpSecurity.httpBasic(withDefaults());
+        return httpSecurity.build();
+    }
+}
