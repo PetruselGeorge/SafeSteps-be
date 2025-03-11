32b8aa970ea9b1b141c2dd951026d4ad77f53ddb
petruselgeorge1 <petruselgeorge@gmail.com>
2025-03-11 18:37:51 +0200

    -made the registration endpoint

--- src/main/java/com/example/SafeStep_be/bo/UserBo.java
+++ src/main/java/com/example/SafeStep_be/bo/UserBo.java
@@ -2,7 +2,6 @@ package com.example.SafeStep_be.bo;
 
 import com.example.SafeStep_be.data.access.layer.entities.UserEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Service;
 
 public interface UserBo extends UserDetailsService {
     void createUser(UserEntity user);
