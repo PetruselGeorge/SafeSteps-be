2cab13eb0b126d80513a36c15d52cffa90772544
petruselgeorge1 <petruselgeorge@gmail.com>
2025-03-13 15:29:02 +0200

    -added the validation for the registration

--- src/main/java/com/example/SafeStep_be/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/SafeStep_be/config/exception/GlobalExceptionHandler.java
@@ -1,20 +1,21 @@
 package com.example.SafeStep_be.config.exception;
 
 import io.micrometer.common.lang.NonNull;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.*;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
